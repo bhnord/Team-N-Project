f94164515e7301b4eed074ba636c940beebc36ac
Michael Emerson <emerson.michael12@gmail.com>
2021-04-03 20:18:57 -0500

    Added Database References

--- src/main/java/edu/wpi/teamname/services/database/Ndb.java
+++ src/main/java/edu/wpi/teamname/services/database/Ndb.java
@@ -1,53 +1,67 @@
 package edu.wpi.teamname.services.database;
 
+import java.sql.*;
+
 public class Ndb {
 
-    public static void main(String[] args) {
-        if (args.length == 0) {
-            System.out.println(
-                    "1 - Node Information\n2 - Update Node Information\n3 - Update Node Location Long Name");
-            System.out.println("4 - Edge Information\n5 - Exit Program");
-        } else {
-            switch (args[0]) {
-                case "1":
-                    // TODO List nodes
-                    break;
-                case "2":
-                    // TODO Update Node Coordinates
-                    break;
-                case "3":
-                    // TODO Update Node Location Long Name
-                    break;
-                case "4":
-                    // TODO Edge Information
-                    break;
-                default:
-                    System.exit(0);
-            }
+  public static void main(String[] args) {
 
-            //      Connection connection;
-            //      try {
-            //        connection = DriverManager.getConnection("jdbc:derby:L1Nodes");
-            //      } catch (SQLException e) {
-            //        e.printStackTrace();
-            //        return;
-            //      }
-        }
+    try {
+      Class.forName("org.apache.derby.jdbc.EmbeddedDriver"); // Apache Derby
+    } catch (ClassNotFoundException e) {
+      e.printStackTrace();
     }
 
-    private void displayNodes() {
+    try {
+      DriverManager.getConnection("jdbc:derby:test;create=true");
+    } catch (SQLException e) {
+      e.printStackTrace();
     }
 
-    private void updateNode() {
+    if (args.length == 0) {
+      System.out.println(
+          "1 - Node Information\n2 - Update Node Information\n3 - Update Node Location Long Name");
+      System.out.println("4 - Edge Information\n5 - Exit Program");
+    } else {
+      switch (args[0]) {
+        case "1":
+          // TODO List nodes
+          break;
+        case "2":
+          // TODO Update Node Coordinates
+          break;
+        case "3":
+          // TODO Update Node Location Long Name
+          break;
+        case "4":
+          // TODO Edge Information
+          break;
+        default:
+          System.exit(0);
+      }
 
-        // scanner in w/ id and then prompt for new x and y
+      //      Connection connection;
+      //      try {
+      //        connection = DriverManager.getConnection("jdbc:derby:L1Nodes");
+      //      } catch (SQLException e) {
+      //        e.printStackTrace();
+      //        return;
+      //      }
     }
+  }
 
-    private void updateNodeLongName() {
-        // enter ID and then prompt w/ new long name
-    }
+  private void displayNodes() {}
 
-    private void getEdgeInfo() {
-        // display list of edges w/ attributes
-    }
+  private void updateNode() {
+
+    // scanner in w/ id and then prompt for new x and y
+  }
+
+  private void updateNodeLongName() {
+    // enter ID and then prompt w/ new long name
+  }
+
+  private void getEdgeInfo() {
+    // display list of edges w/ attributes
+  }
 }
