89bdbb45e86d6b7da9bcb92d55d2a12fb4d18614
jhfeiss <jakefeiss@icloud.com>
2021-04-13 13:27:58 -0400

    CSVEditor update

--- src/main/java/edu/wpi/teamname/views/CSVEditor.java
+++ src/main/java/edu/wpi/teamname/views/CSVEditor.java
@@ -1,16 +1,23 @@
 package edu.wpi.teamname.views;
 
 import com.google.inject.Inject;
+import com.jfoenix.controls.JFXListView;
 import edu.wpi.teamname.services.ServiceTwo;
 import edu.wpi.teamname.services.database.DatabaseService;
 import edu.wpi.teamname.state.HomeState;
+
+import java.awt.event.ActionEvent;
 import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import lombok.extern.slf4j.Slf4j;
 
@@ -22,6 +29,14 @@ public class CSVEditor extends masterController implements Initializable {
   @Inject FXMLLoader loader;
   @Inject HomeState state;
   @FXML private Label text;
+
+
+  @FXML
+  private Button HomeView;
+
+  @FXML
+  private JFXListView<Label> listView;
+
 
   private Scene appPrimaryScene;
 
@@ -37,12 +52,32 @@ public class CSVEditor extends masterController implements Initializable {
   }
 
   @Override
-  public void initialize(URL location, ResourceBundle resources) {
+  public void initialize(URL url, ResourceBundle rb) {
     log.debug(state.toString());
+    for(int i=0; i<4; i++){
+      try{
+        Label lbl = new Label("Item" + i);
+        listView.getItems().add(lbl);
+      } catch (Exception ex){
+        Logger.getLogger(CSVEditor.class.getName()).log(Level.SEVERE, null, ex);
+      }
+    }
   }
+
+  @FXML
+  private void load(ActionEvent event){
+
+  }
+
+  @FXML
+  private void addNew(ActionEvent event){
+
+  }
+
 
   @FXML
   public void advanceHome() throws IOException {
     super.advanceHome(loader, appPrimaryScene);
   }
+
 }
