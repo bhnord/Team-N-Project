94f7ffd5e90d4463efd4f9e17f2f0aceb9fa74fb
Bernhard <b.nordemann@aol.com>
2021-04-17 01:12:52 -0400

    added initTables() to DBS fixed small fxml edges naming error

--- src/main/java/edu/wpi/teamname/services/database/DatabaseService.java
+++ src/main/java/edu/wpi/teamname/services/database/DatabaseService.java
@@ -239,4 +239,54 @@ public class DatabaseService {
       return null;
     }
   }
+
+  public void initTables() {
+    try {
+      String str =
+          "CREATE TABLE Nodes( "
+              + "id varchar(25), "
+              + "xcoord INT NOT NULL, "
+              + "ycoord INT NOT NULL, "
+              + "floor varchar(25), "
+              + "building varchar(25), "
+              + "nodeType varchar(25), "
+              + "longName varchar(45), "
+              + "shortName varchar(35), "
+              + "PRIMARY KEY (id))";
+      stmt.execute(str);
+      str =
+          "CREATE TABLE Edges( "
+              + "id varchar(25), "
+              + "startNodeID varchar(25) REFERENCES Nodes (id), "
+              + "endNodeID varchar(25) REFERENCES Nodes (id), "
+              + "PRIMARY KEY (id))";
+
+      stmt.execute(str);
+      str =
+          "CREATE TABLE Users("
+              + "id INT NOT NULL GENERATED ALWAYS AS IDENTITY,"
+              + "Username varchar(40) NOT NULL UNIQUE, "
+              + "Password varchar(40) NOT NULL,"
+              + "UserType varchar(15),"
+              + "CONSTRAINT chk_UserType CHECK (UserType IN ('Patient', 'Employee', 'Administrator')),"
+              + "PRIMARY KEY (id))";
+      stmt.execute(str);
+      str =
+          "CREATE TABLE Requests("
+              + "id INT NOT NULL GENERATED ALWAYS AS IDENTITY, "
+              + "Type varchar(30), "
+              + "SenderID INT NOT NULL REFERENCES Users (id), "
+              + "ReceiverID INT REFERENCES Users (id), "
+              + "Content varchar(700), "
+              + "Notes varchar(200), "
+              + "CONSTRAINT chk_Type CHECK (Type IN "
+              + "('Food Delivery', 'Language Interpreter', 'Sanitation', 'Laundry', 'Gift Delivery', 'Floral Delivery', 'Medicine Delivery', "
+              + "'Religious Request', 'Internal Patient Transportation', 'External Patient Transportation', 'Security', 'Facilities Maintenance', "
+              + "'Computer Service', 'Audio/Visual')),"
+              + "PRIMARY KEY (id))";
+      stmt.execute(str);
+    } catch (SQLException e) {
+      e.printStackTrace();
+    }
+  }
 }
