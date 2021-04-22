dc0449855134bf322e85c43d38d312e894c6642f
ananya <ananya.gopalan@gmail.com>
2021-04-22 14:58:27 -0400

    updated floral

--- src/main/java/edu/wpi/teamname/views/ComputerServiceRequestController.java
+++ src/main/java/edu/wpi/teamname/views/ComputerServiceRequestController.java
@@ -1,10 +1,7 @@
 package edu.wpi.teamname.views;
 
 import com.google.inject.Inject;
-import com.jfoenix.controls.JFXButton;
-import com.jfoenix.controls.JFXDialog;
-import com.jfoenix.controls.JFXDialogLayout;
-import com.jfoenix.controls.JFXTextField;
+import com.jfoenix.controls.*;
 import com.jfoenix.validation.RequiredFieldValidator;
 import edu.wpi.teamname.services.database.DatabaseService;
 import edu.wpi.teamname.state.HomeState;
@@ -17,12 +14,17 @@ import javafx.event.EventHandler;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.geometry.Insets;
+import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.effect.BoxBlur;
 import javafx.scene.layout.StackPane;
+import javafx.scene.layout.VBox;
 import javafx.scene.text.Text;
 import javafx.stage.Stage;
+import lombok.SneakyThrows;
 import lombok.extern.slf4j.Slf4j;
 
 @Slf4j
@@ -32,14 +34,19 @@ public class ComputerServiceRequestController extends masterController implement
   @Inject FXMLLoader loader;
   @Inject HomeState state;
   @FXML private Label text;
-  @FXML private JFXTextField txtEmployeeName;
-  @FXML private JFXTextField txtStartRoom;
-  @FXML private JFXTextField txtEndRoom;
-  private Scene appPrimaryScene;
+  @FXML private Label errorLabel;
+  @FXML private JFXTextField txtRoom;
+  @FXML private JFXTextField txtTimeOfRequest;
+  @FXML private JFXTextField txtEquipment;
+  @FXML private JFXTextField txtComments;
   @FXML private Button helpButton;
   @FXML private StackPane myStackPane;
-  String helpPagePath = "ComputerRequestHelpPage";
-  Stage primaryStage;
+  @FXML private Button submit;
+  @FXML private StackPane myStackPane2;
+  private Scene appPrimaryScene;
+  @FXML JFXComboBox<String> txtEmployeeName = new JFXComboBox<>();
+  static Stage stage;
+
   /**
    * This method allows the tests to inject the scene at a later time, since it must be done on the
    * JavaFX thread
@@ -54,32 +61,59 @@ public class ComputerServiceRequestController extends masterController implement
   @Override
   public void initialize(URL location, ResourceBundle resources) {
     log.debug(state.toString());
+
+    txtEmployeeName.getItems().add("person1");
+    txtEmployeeName.getItems().add("person2");
+
     /** USERNAME input and password* */
     RequiredFieldValidator reqInputValid = new RequiredFieldValidator();
-    reqInputValid.setMessage("Cannot be empty");
+    /* reqInputValid.setMessage("Cannot be empty");
     txtEmployeeName.getValidators().add(reqInputValid);
     txtEmployeeName
         .focusedProperty()
         .addListener(
             (o, oldVal, newVal) -> {
               if (!newVal) txtEmployeeName.validate();
-            });
+            });*/
     reqInputValid.setMessage("Cannot be empty");
-    txtStartRoom.getValidators().add(reqInputValid);
-    txtStartRoom
+    txtRoom.getValidators().add(reqInputValid);
+    txtRoom
         .focusedProperty()
         .addListener(
             (o, oldVal, newVal) -> {
-              if (!newVal) txtStartRoom.validate();
+              if (!newVal) txtRoom.validate();
             });
     reqInputValid.setMessage("Cannot be empty");
-    txtEndRoom.getValidators().add(reqInputValid);
-    txtEndRoom
+    txtEquipment.getValidators().add(reqInputValid);
+    txtEquipment
         .focusedProperty()
         .addListener(
             (o, oldVal, newVal) -> {
-              if (!newVal) txtEndRoom.validate();
+              if (!newVal) txtEquipment.validate();
             });
+  }
+
+  public void exit(ActionEvent actionEvent) throws IOException {
+    super.cancel(actionEvent);
+  }
+
+  @FXML
+  public void logOut() throws IOException {
+    super.logOut(loader, appPrimaryScene);
+  }
+
+  @FXML
+  public void advanceHome() throws IOException {
+
+    Login login = Login.getLogin();
+
+    if (login.getUsername().equals("p") && login.getPassword().equals("p")) {
+      super.advanceHomePatient(loader, appPrimaryScene);
+    } else if (login.getUsername().equals("e") && login.getPassword().equals("e")) {
+      super.advanceHome(loader, appPrimaryScene);
+    } else if (login.getUsername().equals("a") && login.getPassword().equals("a")) {
+      super.advanceHomeAdmin(loader, appPrimaryScene);
+    }
   }
 
   @FXML
@@ -96,36 +130,67 @@ public class ComputerServiceRequestController extends masterController implement
     }
   }
 
-  @FXML
-  public void advanceHome() throws IOException {
-    // super.advanceServiceRequest(loader, appPrimaryScene);
-    Login login = Login.getLogin();
-
-    if (login.getUsername().equals("p") && login.getPassword().equals("p")) {
-      super.advanceHomePatient(loader, appPrimaryScene);
-    } else if (login.getUsername().equals("e") && login.getPassword().equals("e")) {
-      super.advanceHome(loader, appPrimaryScene);
-    } else if (login.getUsername().equals("a") && login.getPassword().equals("a")) {
-      super.advanceHomeAdmin(loader, appPrimaryScene);
-    }
-  }
-
   public void Submit(ActionEvent actionEvent) throws IOException {
-    ConfirmBoxComputer.confirm(this);
+
+    VBox manuContainer = new VBox();
+    Label lbl1 = new Label("Are you sure the information you have provided is correct?");
+
+    JFXButton continueButton = new JFXButton("Continue");
+    continueButton.setButtonType(JFXButton.ButtonType.RAISED);
+    continueButton.setStyle("-fx-background-color : #00bfff:");
+
+    JFXButton cancelButton = new JFXButton("Cancel");
+    cancelButton.setButtonType(JFXButton.ButtonType.RAISED);
+    cancelButton.setStyle("-fx-background-color : #00bfff:");
+
+    cancelButton.setTranslateX(100);
+    cancelButton.setTranslateY(65);
+
+    continueButton.setTranslateX(200);
+    continueButton.setTranslateY(25);
+
+    manuContainer.getChildren().addAll(lbl1, cancelButton, continueButton);
+    manuContainer.setPadding(new Insets(30, 50, 50, 50));
+    manuContainer.setSpacing(10);
+    JFXPopup popup1 = new JFXPopup(manuContainer);
+
+    cancelButton.setOnAction(
+        new EventHandler<ActionEvent>() {
+          @Override
+          public void handle(ActionEvent event) {
+            popup1.hide();
+            submit.setDisable(false);
+          }
+        });
+
+    continueButton.setOnAction(
+        new EventHandler<ActionEvent>() {
+          @SneakyThrows
+          @Override
+          public void handle(ActionEvent event) {
+            popup1.hide();
+            Parent root =
+                loader.load(getClass().getResourceAsStream("ConfirmationPageComputer.fxml"));
+            appPrimaryScene.setRoot(root);
+            submit.setDisable(false);
+          }
+        });
+    submit.setDisable(true);
+    popup1.show(myStackPane2, JFXPopup.PopupVPosition.BOTTOM, JFXPopup.PopupHPosition.LEFT);
   }
 
   public void help(ActionEvent actionEvent) throws IOException {
-
     String title = "Help Page";
+    BoxBlur blur = new BoxBlur(3, 3, 3);
     JFXDialogLayout dialogContent = new JFXDialogLayout();
     dialogContent.setHeading(new Text(title));
     dialogContent.setBody(
         (new Text(
             "* Employee Name refers to the employee being requested to complete the job\n"
-                + "* Hospital Location refers to the area of the hospital in need of computer service\n"
-                + "* Comments refer to additional information pertaining to the service\n"
-                + "* Time of Request refers to the time the service was submitted\n"
-                + "* Name refers to the name of the person in need of computer service\n")));
+                + "* Patient Room is the room that the employee will deliver the medicine to\n"
+                + "* Time of request refers to time the medicine should be delivered to the patient\n"
+                + "* Necessary Equipment refers to additional services/equipment the patient requires\n"
+                + "* Necessary Equipment refers to additional services/equipment the patient requires\n")));
     JFXButton close = new JFXButton("close");
     close.setButtonType(JFXButton.ButtonType.RAISED);
     close.setStyle("-fx-background-color : #00bfff:");
@@ -136,11 +201,13 @@ public class ComputerServiceRequestController extends masterController implement
         new EventHandler<ActionEvent>() {
           @Override
           public void handle(ActionEvent event) {
+            // anchorPage.setEffect(null);
             dialog.close();
             helpButton.setDisable(false);
           }
         });
     helpButton.setDisable(true);
     dialog.show();
+    // anchorPage.setEffect(blur);
   }
 }
--- src/main/java/edu/wpi/teamname/views/ConfirmBoxFloral.java
+++ src/main/java/edu/wpi/teamname/views/ConfirmBoxFloral.java
@@ -1,57 +1,22 @@
 package edu.wpi.teamname.views;
 
+import edu.wpi.teamname.state.Login;
 import java.io.IOException;
 import javafx.fxml.FXML;
-import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
-import javafx.scene.Parent;
-import javafx.scene.Scene;
-import javafx.stage.Modality;
-import javafx.stage.Stage;
 
 public class ConfirmBoxFloral extends masterController implements Initializable {
-  static Stage stage;
-  static ConfirmBoxFloral box;
-  static FloralRequestController floral = new FloralRequestController();
-
-  public static void confirm(FloralRequestController floralType) throws IOException {
-
-    floral = floralType;
-    String title = "floral confirmation box";
-
-    FXMLLoader fxmlLoader =
-        new FXMLLoader(ConfirmBoxFood.class.getResource("ConfirmBoxFloral.fxml"));
-    Parent root = fxmlLoader.load();
-    Scene scene = new Scene(root);
-    box = fxmlLoader.getController();
-    box.stage = new Stage();
-    box.stage.setScene(scene);
-
-    box.stage.initModality(
-        Modality.APPLICATION_MODAL); // pop-up window won't close until you have dealt with it
-    box.stage.setAlwaysOnTop(true);
-    box.stage.setTitle(title);
-
-    box.stage.showAndWait();
-  }
-
   @FXML
-  public void cancel() throws IOException {
-    box.stage.close();
-  }
+  public void advanceHome() throws IOException {
 
-  @FXML
-  public void continuePage() throws IOException {
-    FXMLLoader fxmlLoader =
-        new FXMLLoader(ConfirmBoxFood.class.getResource("ConfirmationPageFloral.fxml"));
-    Parent root = fxmlLoader.load();
-    Scene scene = new Scene(root);
-    box.stage.setScene(scene);
-  }
+    Login login = Login.getLogin();
 
-  @FXML
-  public void returnToHomeFloral() throws IOException {
-    box.stage.close();
-    floral.advanceHome();
+    if (login.getUsername().equals("p") && login.getPassword().equals("p")) {
+      super.advanceHomePatient(loader, appPrimaryScene);
+    } else if (login.getUsername().equals("e") && login.getPassword().equals("e")) {
+      super.advanceHome(loader, appPrimaryScene);
+    } else if (login.getUsername().equals("a") && login.getPassword().equals("a")) {
+      super.advanceHomeAdmin(loader, appPrimaryScene);
+    }
   }
 }
