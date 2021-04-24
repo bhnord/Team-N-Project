03d511eccffda4643d212233ba776b00c6a2166b
Bernhard <b.nordemann@aol.com>
2021-04-23 20:54:22 -0400

    refactored NodesTable

--- src/main/java/edu/wpi/TeamN/services/database/DatabaseService.java
+++ src/main/java/edu/wpi/TeamN/services/database/DatabaseService.java
@@ -24,7 +24,7 @@ public class DatabaseService {
   private final Connection connection;
   private Statement stmt;
   @Inject UsersTable usersTable;
-
+  @Inject NodesTable nodesTable;
 
   @Inject
   DatabaseService(Connection connection) {
@@ -36,95 +36,23 @@ public class DatabaseService {
     }
   }
 
+  /// NODES
   public HashSet<Node> getAllNodes() {
-    String query = "SELECT * FROM NODES";
-    try {
-      ResultSet nodes = stmt.executeQuery(query);
-      return resultSetToNodes(nodes);
-    } catch (SQLException e) {
-      e.printStackTrace();
-      return null;
-    }
+    return nodesTable.getAllNodes();
   }
 
   public HashMap<String, Node> getAllNodesMap() {
-    HashSet<Node> nodeSet = getAllNodes();
-    HashMap<String, Node> nodeMap = new HashMap<>();
-    for (Node node : nodeSet) {
-      nodeMap.put(node.get_nodeID(), node);
-    }
-    return nodeMap;
+    return nodesTable.getAllNodesMap();
   }
 
-  /**
-   * gets node from the NODES table
-   *
-   * @param nodeID
-   * @return node from table
-   */
   public Node getNode(String nodeID) {
-    String query = "SELECT * FROM NODES WHERE id = '" + nodeID + "'";
-    try {
-      ResultSet rs = stmt.executeQuery(query);
-      HashSet<Node> set = resultSetToNodes(rs);
-      if (set.size() > 0) {
-        return (Node) set.toArray()[0];
-      } else {
-        return null;
-      }
-    } catch (SQLException e) {
-      e.printStackTrace();
-      return null;
-    }
+    return nodesTable.getNode(nodeID);
   }
 
-  /**
-   * adds given node to the NODES table
-   *
-   * @param node the node to add to table
-   * @return whether operation was carried out successfully
-   */
   public boolean addNode(Node node) {
-    String query =
-        "INSERT INTO NODES VALUES ('"
-            + node.get_nodeID()
-            + "', "
-            + node.get_x()
-            + ", "
-            + node.get_y()
-            + ", '"
-            + node.get_floor()
-            + "', '"
-            + node.get_building()
-            + "', '"
-            + node.get_nodeType()
-            + "', '"
-            + node.get_longName()
-            + "', '"
-            + node.get_shortName()
-            + "')";
-    try {
-      stmt.execute(query);
-      return true;
-    } catch (SQLException e) {
-      e.printStackTrace();
-      return false;
-    }
+    return nodesTable.addNode(node);
   }
 
-  /**
-   * updates selected node data (id stays the same.)
-   *
-   * @param id
-   * @param x
-   * @param y
-   * @param floor
-   * @param building
-   * @param nodeType
-   * @param longName
-   * @param shortName
-   * @return
-   */
   public boolean updateNode(
       String id,
       double x,
@@ -134,57 +62,15 @@ public class DatabaseService {
       String nodeType,
       String longName,
       String shortName) {
-    String str =
-        "UPDATE NODES SET xcoord = "
-            + x
-            + ",ycoord = "
-            + y
-            + ", floor = "
-            + floor
-            + ", building = "
-            + building
-            + ", nodeType = "
-            + nodeType
-            + ", longName = "
-            + longName
-            + ", shortName = "
-            + shortName
-            + " WHERE id ="
-            + id;
-    try {
-      stmt.execute(str);
-      return true;
-    } catch (SQLException e) {
-      e.printStackTrace();
-      return false;
-    }
+    return nodesTable.updateNode(id, x, y, floor, building, nodeType, longName, shortName);
   }
 
-  /** deletes all rows from NODES table */
   public void deleteNodeRows() {
-    String str = "DELETE FROM NODES";
-    try {
-      stmt.execute(str);
-    } catch (SQLException e) {
-      e.printStackTrace();
-    }
+    nodesTable.deleteNodeRows();
   }
 
-  /**
-   * deletes node from DB - WILL ALSO DELETE ANY ASSOCIATED EDGES -
-   *
-   * @param nodeID node's id
-   * @return whether the operation was carried out successfully
-   */
   public boolean deleteNode(String nodeID) {
-    String st = "DELETE FROM NODES WHERE id = '" + nodeID + "'";
-    try {
-      stmt.execute(st);
-      return true;
-    } catch (SQLException e) {
-      e.printStackTrace();
-      return false;
-    }
+    return nodesTable.deleteNode(nodeID);
   }
 
   public boolean addEdge(Edge e) {
@@ -425,24 +311,25 @@ public class DatabaseService {
   }
 
   //////////////////////////
-  boolean addUser(User user, String password){
+  boolean addUser(User user, String password) {
     return usersTable.addUser(user, password);
   }
+
   public User getUserById(String id) {
     return usersTable.getUserById(id);
   }
-  public User getUserByUsername(String username){
+
+  public User getUserByUsername(String username) {
     return usersTable.getUserByUsername(username);
   }
+
   public HashMap<String, User> getUsersByType(UserType userType) {
     return usersTable.getUsersByType(userType);
   }
+
   public boolean deleteUser(String username) {
     return usersTable.deleteUser(username);
   }
-
-
-
 
   private HashSet<Request> resultSetToRequest(ResultSet rs) throws SQLException {
     HashSet<Request> requests = new HashSet<>();
@@ -458,27 +345,6 @@ public class DatabaseService {
               rs.getString("NOTES")));
     }
     return requests;
-  }
-
-  private HashSet<Node> resultSetToNodes(ResultSet rs) {
-    HashSet<Node> nodeSet = new HashSet<>();
-    try {
-      while (rs.next()) {
-        String nodeID = rs.getString("id");
-        String floor = rs.getString("FLOOR");
-        String building = rs.getString("BUILDING");
-        String nodeType = rs.getString("NODETYPE");
-        String longName = rs.getString("LONGNAME");
-        String shortName = rs.getString("SHORTNAME");
-        double xPos = rs.getDouble("XCOORD");
-        double yPos = rs.getDouble("YCOORD");
-        nodeSet.add(new Node(xPos, yPos, nodeID, floor, building, nodeType, longName, shortName));
-      }
-      return nodeSet;
-    } catch (SQLException e) {
-      e.printStackTrace();
-      return null;
-    }
   }
 
   private HashSet<Edge> resultSetToEdges(ResultSet rs) {
--- src/main/java/edu/wpi/TeamN/services/database/UsersTable.java
+++ src/main/java/edu/wpi/TeamN/services/database/UsersTable.java
@@ -2,7 +2,6 @@ package edu.wpi.TeamN.services.database;
 
 import com.google.inject.Inject;
 import edu.wpi.TeamN.services.database.users.*;
-
 import java.sql.Connection;
 import java.sql.ResultSet;
 import java.sql.SQLException;
@@ -11,114 +10,109 @@ import java.util.HashMap;
 import java.util.HashSet;
 
 public class UsersTable {
-    private static UsersTable table;
-    private final Connection connection;
-    private Statement stmt;
+  private final Connection connection;
+  private Statement stmt;
 
-
-    @Inject
-    UsersTable(Connection connection) {
-        this.connection = connection;
-        try {
-            this.stmt = connection.createStatement();
-        } catch (SQLException e) {
-            e.printStackTrace();
-        }
+  @Inject
+  UsersTable(Connection connection) {
+    this.connection = connection;
+    try {
+      this.stmt = connection.createStatement();
+    } catch (SQLException e) {
+      e.printStackTrace();
     }
+  }
 
-
-    public boolean addUser(User user, String password) {
-        String str =
-                "INSERT INTO USERS VALUES ("
-                        + user.getId()
-                        + ", '"
-                        + user.getUsername()
-                        + "', '"
-                        + password
-                        + "', '"
-                        + user.getType().toString()
-                        + "')";
-        try {
-            stmt.execute(str);
-            return true;
-        } catch (SQLException e) {
-            e.printStackTrace();
-            return false;
-        }
+  public boolean addUser(User user, String password) {
+    String str =
+        "INSERT INTO USERS VALUES ("
+            + user.getId()
+            + ", '"
+            + user.getUsername()
+            + "', '"
+            + password
+            + "', '"
+            + user.getType().toString()
+            + "')";
+    try {
+      stmt.execute(str);
+      return true;
+    } catch (SQLException e) {
+      e.printStackTrace();
+      return false;
     }
+  }
 
-    public User getUserById(String id) {
-        String str = "SELECT * FROM USERS WHERE ID = " + id;
-        try {
-            ResultSet rs = stmt.executeQuery(str);
-            return (User) resultSetToUsers(rs).toArray()[0];
-        } catch (SQLException e) {
-            return null;
-        }
+  public User getUserById(String id) {
+    String str = "SELECT * FROM USERS WHERE ID = " + id;
+    try {
+      ResultSet rs = stmt.executeQuery(str);
+      return (User) resultSetToUsers(rs).toArray()[0];
+    } catch (SQLException e) {
+      return null;
     }
+  }
 
-    public User getUserByUsername(String username) {
-        String str = "SELECT * FROM USERS WHERE USERNAME = " + username;
-        try {
-            ResultSet rs = stmt.executeQuery(str);
-            return (User) resultSetToUsers(rs).toArray()[0];
-        } catch (SQLException e) {
-            e.printStackTrace();
-            return null;
-        }
+  public User getUserByUsername(String username) {
+    String str = "SELECT * FROM USERS WHERE USERNAME = " + username;
+    try {
+      ResultSet rs = stmt.executeQuery(str);
+      return (User) resultSetToUsers(rs).toArray()[0];
+    } catch (SQLException e) {
+      e.printStackTrace();
+      return null;
     }
+  }
 
-    public HashMap<String, User> getUsersByType(UserType userType) {
-        String userTypeString =
-                userType.toString().charAt(0) + userType.toString().substring(1).toLowerCase();
-        String str = "SELECT * FROM USERS WHERE USERTYPE = '" + userTypeString + "'";
-        HashMap<String, User> userMap = new HashMap<>();
-        try {
-            ResultSet rs = stmt.executeQuery(str);
-            HashSet<User> usersSet = resultSetToUsers(rs);
-            assert usersSet != null;
-            for (User user : usersSet) {
-                userMap.put(user.getId(), user);
-            }
-            return userMap;
-        } catch (SQLException e) {
-            e.printStackTrace();
-            return userMap;
-        }
+  public HashMap<String, User> getUsersByType(UserType userType) {
+    String userTypeString =
+        userType.toString().charAt(0) + userType.toString().substring(1).toLowerCase();
+    String str = "SELECT * FROM USERS WHERE USERTYPE = '" + userTypeString + "'";
+    HashMap<String, User> userMap = new HashMap<>();
+    try {
+      ResultSet rs = stmt.executeQuery(str);
+      HashSet<User> usersSet = resultSetToUsers(rs);
+      assert usersSet != null;
+      for (User user : usersSet) {
+        userMap.put(user.getId(), user);
+      }
+      return userMap;
+    } catch (SQLException e) {
+      e.printStackTrace();
+      return userMap;
     }
+  }
 
-    public boolean deleteUser(String username) {
-        String str = "DELETE USERS WHERE USERNAME = " + username;
-        try {
-            stmt.execute(str);
-            return true;
-        } catch (SQLException e) {
-            return false;
-        }
+  public boolean deleteUser(String username) {
+    String str = "DELETE USERS WHERE USERNAME = " + username;
+    try {
+      stmt.execute(str);
+      return true;
+    } catch (SQLException e) {
+      return false;
     }
+  }
 
-    private HashSet<User> resultSetToUsers(ResultSet rs) {
-        HashSet<User> users = new HashSet<>();
-        try {
-            while (rs.next()) {
-                switch (rs.getString("USERTYPE")) {
-                    case "Patient":
-                        users.add(new Patient(rs.getString("ID"), rs.getString("USERNAME")));
-                        break;
-                    case "Employee":
-                        users.add(new Employee(rs.getString("ID"), rs.getString("USERNAME")));
-                        break;
-                    case "Administrator":
-                        users.add(new Administrator(rs.getString("ID"), rs.getString("USERNAME")));
-                        break;
-                }
-            }
-            return users;
-        } catch (SQLException e) {
-            e.printStackTrace();
-            return null;
+  private HashSet<User> resultSetToUsers(ResultSet rs) {
+    HashSet<User> users = new HashSet<>();
+    try {
+      while (rs.next()) {
+        switch (rs.getString("USERTYPE")) {
+          case "Patient":
+            users.add(new Patient(rs.getString("ID"), rs.getString("USERNAME")));
+            break;
+          case "Employee":
+            users.add(new Employee(rs.getString("ID"), rs.getString("USERNAME")));
+            break;
+          case "Administrator":
+            users.add(new Administrator(rs.getString("ID"), rs.getString("USERNAME")));
+            break;
         }
+      }
+      return users;
+    } catch (SQLException e) {
+      e.printStackTrace();
+      return null;
     }
-
-
+  }
 }
