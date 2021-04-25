f3dbbdcf178e4c2dd3a5f00fbcb6977529c4af26
Michael Emerson <emerson.michael12@gmail.com>
2021-04-25 17:09:02 -0400

    Working on ext patient

--- src/main/java/edu/wpi/TeamN/views/ExternalPatientRequestController.java
+++ src/main/java/edu/wpi/TeamN/views/ExternalPatientRequestController.java
@@ -1,7 +1,9 @@
 package edu.wpi.TeamN.views;
 
 import com.google.inject.Inject;
-import com.google.maps.*;
+import com.google.maps.GeoApiContext;
+import com.google.maps.PlaceAutocompleteRequest;
+import com.google.maps.PlacesApi;
 import com.google.maps.errors.ApiException;
 import com.google.maps.model.AutocompletePrediction;
 import com.google.maps.model.LatLng;
@@ -9,20 +11,24 @@ import com.jfoenix.controls.*;
 import com.jfoenix.validation.RequiredFieldValidator;
 import edu.wpi.TeamN.services.algo.Node;
 import edu.wpi.TeamN.services.database.DatabaseService;
+import edu.wpi.TeamN.services.database.requests.Request;
+import edu.wpi.TeamN.services.database.requests.RequestType;
 import edu.wpi.TeamN.services.database.users.User;
 import edu.wpi.TeamN.services.database.users.UserType;
 import edu.wpi.TeamN.state.HomeState;
 import edu.wpi.TeamN.state.Login;
 import java.io.IOException;
 import java.net.URL;
-import java.util.*;
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.ResourceBundle;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
 import javafx.geometry.Insets;
-import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
@@ -38,27 +44,26 @@ import lombok.extern.slf4j.Slf4j;
 @Slf4j
 public class ExternalPatientRequestController extends masterController implements Initializable {
 
+  // @FXML private AnchorPane anchorPage;
+  static Stage stage;
   @Inject DatabaseService db;
   @Inject FXMLLoader loader;
   @Inject HomeState state;
-  @FXML private Label text;
   @FXML private Label errorLabel;
-  private Label person1;
-  @FXML private JFXTextField txtTimeOfRequest;
-  @FXML private JFXTextField txtEquipment;
-  @FXML private JFXTextField txtComments;
+  @FXML private JFXTextField commentsBox;
   @FXML private Button helpButton;
   @FXML private StackPane myStackPane;
   @FXML private Button submit;
   @FXML private StackPane myStackPane2;
-  @FXML private JFXComboBox addressBox = new JFXComboBox();
+  @FXML private JFXComboBox addressBox;
+  @FXML private JFXComboBox transportTypeDropdown;
+  @FXML private JFXComboBox<Label> employeeDropdown;
+  @FXML private JFXComboBox<Label> patientRoomDropdown;
+  @FXML private JFXTimePicker departureTIme;
   private Scene appPrimaryScene;
+
   private HashMap<String, User> users;
   private HashMap<String, Node> rooms;
-  @FXML private JFXComboBox<Label> txtEmployeeName = new JFXComboBox<>();
-  @FXML private JFXComboBox<Label> roomDropdown = new JFXComboBox<>();
-  // @FXML private AnchorPane anchorPage;
-  static Stage stage;
 
   /**
    * This method allows the tests to inject the scene at a later time, since it must be done on the
@@ -75,37 +80,13 @@ public class ExternalPatientRequestController extends masterController implement
   public void initialize(URL location, ResourceBundle resources) {
     log.debug(state.toString());
     submit.setDisable(true);
-
-    /** USERNAME input and password* */
-    RequiredFieldValidator reqInputValid = new RequiredFieldValidator();
-    reqInputValid.setMessage("Cannot be empty");
-    txtTimeOfRequest.getValidators().add(reqInputValid);
-    txtTimeOfRequest
-        .focusedProperty()
-        .addListener(
-            (o, oldVal, newVal) -> {
-              if (!newVal) txtTimeOfRequest.validate();
-            });
-    reqInputValid.setMessage("Cannot be empty");
-    txtComments.getValidators().add(reqInputValid);
-    txtComments
-        .focusedProperty()
-        .addListener(
-            (o, oldVal, newVal) -> {
-              if (!newVal) txtComments.validate();
-            });
-    reqInputValid.setMessage("Cannot be empty");
-    txtEquipment.getValidators().add(reqInputValid);
-    txtEquipment
-        .focusedProperty()
-        .addListener(
-            (o, oldVal, newVal) -> {
-              if (!newVal) txtEquipment.validate();
-            });
-
+    transportTypeDropdown.getItems().add("Ambulance");
+    transportTypeDropdown.getItems().add("Helicopter");
+    transportTypeDropdown.getItems().add("Plane");
     loadEmployeeDropdown();
     loadRoomDropdown();
     loadMapsStuff();
+    setupValidation();
   }
 
   public void exit(ActionEvent actionEvent) throws IOException {
@@ -147,20 +128,16 @@ public class ExternalPatientRequestController extends masterController implement
 
   public void Submit(ActionEvent actionEvent) throws IOException {
 
-    txtEmployeeName.setValidators();
-    if (txtEmployeeName.getSelectionModel().isEmpty() || roomDropdown.getSelectionModel().isEmpty())
-      return;
-
-    VBox manuContainer = new VBox();
+    VBox menuContainer = new VBox();
     Label lbl1 = new Label("Are you sure the information you have provided is correct?");
 
     JFXButton continueButton = new JFXButton("Continue");
     continueButton.setButtonType(JFXButton.ButtonType.RAISED);
-    continueButton.setStyle("-fx-background-color : #00bfff:");
+    continueButton.setStyle("-fx-background-color : #00bfff");
 
     JFXButton cancelButton = new JFXButton("Cancel");
     cancelButton.setButtonType(JFXButton.ButtonType.RAISED);
-    cancelButton.setStyle("-fx-background-color : #00bfff:");
+    cancelButton.setStyle("-fx-background-color : #00bfff");
 
     cancelButton.setTranslateX(100);
     cancelButton.setTranslateY(65);
@@ -168,18 +145,15 @@ public class ExternalPatientRequestController extends masterController implement
     continueButton.setTranslateX(200);
     continueButton.setTranslateY(25);
 
-    manuContainer.getChildren().addAll(lbl1, cancelButton, continueButton);
-    manuContainer.setPadding(new Insets(30, 50, 50, 50));
-    manuContainer.setSpacing(10);
-    JFXPopup popup1 = new JFXPopup(manuContainer);
+    menuContainer.getChildren().addAll(lbl1, cancelButton, continueButton);
+    menuContainer.setPadding(new Insets(30, 50, 50, 50));
+    menuContainer.setSpacing(10);
+    JFXPopup popup1 = new JFXPopup(menuContainer);
 
     cancelButton.setOnAction(
-        new EventHandler<ActionEvent>() {
-          @Override
-          public void handle(ActionEvent event) {
-            popup1.hide();
-            submit.setDisable(false);
-          }
+        event -> {
+          popup1.hide();
+          submit.setDisable(false);
         });
 
     continueButton.setOnAction(
@@ -188,10 +162,14 @@ public class ExternalPatientRequestController extends masterController implement
           @Override
           public void handle(ActionEvent event) {
             popup1.hide();
-            Parent root =
-                loader.load(getClass().getResourceAsStream("ConfirmationPageExternal.fxml"));
-            appPrimaryScene.setRoot(root);
-            submit.setDisable(false);
+            //            Parent root =
+            //
+            // loader.load(getClass().getResourceAsStream("ConfirmationPageExternal.fxml"));
+            //            appPrimaryScene.setRoot(root);
+            if (submitToDatabase()) {
+              submit.setDisable(false);
+              errorLabel.setText("Submitted!");
+            } else errorLabel.setText("Something went wrong");
           }
         });
     submit.setDisable(true);
@@ -200,9 +178,12 @@ public class ExternalPatientRequestController extends masterController implement
 
   @FXML
   private void validateButton() {
-    if (!txtTimeOfRequest.getText().isEmpty()
-        && !txtEquipment.getText().isEmpty()
-        && !txtComments.getText().isEmpty()) {
+    if (!departureTIme.getEditor().getText().isEmpty()
+        && !transportTypeDropdown.getSelectionModel().isEmpty()
+        && !patientRoomDropdown.getEditor().getText().isEmpty()
+        && !employeeDropdown.getEditor().getText().isEmpty()
+        && !addressBox.getEditor().getText().isEmpty()
+        && !commentsBox.getText().isEmpty()) {
       submit.setDisable(false);
     } else {
       submit.setDisable(true);
@@ -223,7 +204,7 @@ public class ExternalPatientRequestController extends masterController implement
                 + "* Necessary Equipment refers to additional services/equipment the patient requires\n")));
     JFXButton close = new JFXButton("close");
     close.setButtonType(JFXButton.ButtonType.RAISED);
-    close.setStyle("-fx-background-color : #00bfff:");
+    close.setStyle("-fx-background-color : #00bfff");
     dialogContent.setActions(close);
 
     JFXDialog dialog = new JFXDialog(myStackPane, dialogContent, JFXDialog.DialogTransition.BOTTOM);
@@ -241,14 +222,28 @@ public class ExternalPatientRequestController extends masterController implement
     // anchorPage.setEffect(blur);
   }
 
+  private boolean submitToDatabase() {
+    Request request =
+        new Request(
+            RequestType.EXTERNAL_PATIENT_TRANSPORTATION,
+            Integer.parseInt(employeeDropdown.getSelectionModel().getSelectedItem().getId()),
+            patientRoomDropdown.getSelectionModel().getSelectedItem().getId(),
+            "Transportation Method: "
+                + transportTypeDropdown.getSelectionModel().getSelectedItem()
+                + " Destination: "
+                + addressBox.getSelectionModel().getSelectedItem(),
+            commentsBox.getText());
+    return db.addRequest(request);
+  }
+
   private void loadEmployeeDropdown() {
     users = db.getUsersByType(UserType.EMPLOYEE);
     for (User user : users.values()) {
       Label lbl = new Label(user.getUsername());
       lbl.setId(user.getId());
-      txtEmployeeName.getItems().add((lbl));
+      employeeDropdown.getItems().add((lbl));
     }
-    new AutoCompleteComboBoxListener(txtEmployeeName);
+    new AutoCompleteComboBoxListener(employeeDropdown);
   }
 
   private void loadRoomDropdown() {
@@ -256,9 +251,9 @@ public class ExternalPatientRequestController extends masterController implement
     for (Node node : rooms.values()) {
       Label lbl = new Label(node.get_longName());
       lbl.setId(node.get_nodeID());
-      roomDropdown.getItems().add(lbl);
+      patientRoomDropdown.getItems().add(lbl);
     }
-    new AutoCompleteComboBoxListener(roomDropdown);
+    new AutoCompleteComboBoxListener(patientRoomDropdown);
   }
 
   private void loadMapsStuff() {
@@ -276,13 +271,9 @@ public class ExternalPatientRequestController extends masterController implement
             predictions =
                 PlacesApi.placeAutocomplete(test, addressBox.getEditor().getText(), token)
                     .origin(origin)
+                    .radius(160934) // 100 miles in meters
                     .await();
-            System.out.println(predictions[0].description);
-          } catch (ApiException e) {
-            e.printStackTrace();
-          } catch (InterruptedException e) {
-            e.printStackTrace();
-          } catch (IOException e) {
+          } catch (ApiException | InterruptedException | IOException e) {
             e.printStackTrace();
           }
           Collection<String> address = new HashSet<>();
@@ -293,15 +284,61 @@ public class ExternalPatientRequestController extends masterController implement
           addressBox.show();
         });
   }
-}
 
-class test {
-  public static void main(String[] args) throws IOException, InterruptedException, ApiException {
-    GeoApiContext test =
-        new GeoApiContext.Builder().apiKey("AIzaSyBBszEPZvetVvgsIbt3pLtXLbPap6dT-KY" + "").build();
-    PlaceAutocompleteRequest.SessionToken token = new PlaceAutocompleteRequest.SessionToken();
-    AutocompletePrediction[] predictions =
-        PlacesApi.placeAutocomplete(test, "3817 Colg", token).await();
-    System.out.println(predictions[0].description);
+  private void setupValidation() {
+    RequiredFieldValidator reqInputValid = new RequiredFieldValidator();
+    reqInputValid.setMessage("Cannot be empty");
+    commentsBox.getValidators().add(reqInputValid);
+    commentsBox
+        .focusedProperty()
+        .addListener(
+            (o, oldVal, newVal) -> {
+              if (!newVal) commentsBox.validate();
+            });
+
+    reqInputValid.setMessage("Cannot be empty");
+    transportTypeDropdown.getValidators().add(reqInputValid);
+    transportTypeDropdown
+        .focusedProperty()
+        .addListener(
+            (o, oldVal, newVal) -> {
+              if (!newVal) transportTypeDropdown.validate();
+            });
+
+    reqInputValid.setMessage("Cannot be empty");
+    patientRoomDropdown.getValidators().add(reqInputValid);
+    patientRoomDropdown
+        .focusedProperty()
+        .addListener(
+            (o, oldVal, newVal) -> {
+              if (!newVal) patientRoomDropdown.validate();
+            });
+
+    reqInputValid.setMessage("Cannot be empty");
+    employeeDropdown.getValidators().add(reqInputValid);
+    employeeDropdown
+        .focusedProperty()
+        .addListener(
+            (o, oldVal, newVal) -> {
+              if (!newVal) employeeDropdown.validate();
+            });
+
+    reqInputValid.setMessage("Cannot be empty");
+    addressBox.getValidators().add(reqInputValid);
+    addressBox
+        .focusedProperty()
+        .addListener(
+            (o, oldVal, newVal) -> {
+              if (!newVal) addressBox.validate();
+            });
+
+    reqInputValid.setMessage("Cannot be empty");
+    departureTIme.getValidators().add(reqInputValid);
+    departureTIme
+        .focusedProperty()
+        .addListener(
+            (o, oldVal, newVal) -> {
+              if (!newVal) departureTIme.validate();
+            });
   }
 }
