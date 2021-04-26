ea43706e733ad5973de24d77c409ddefa6e3cb1f
Michael Emerson <emerson.michael12@gmail.com>
2021-04-26 19:45:15 -0400

    Added UserPrefs (You will need to delete your db and rerun initTables())

--- src/main/java/edu/wpi/TeamN/services/database/users/Guest.java
+++ src/main/java/edu/wpi/TeamN/services/database/users/Guest.java
@@ -1,8 +1,8 @@
 package edu.wpi.TeamN.services.database.users;
 
 public class Guest extends User {
-  public Guest(String id, String username) {
-    super(id, username);
+  public Guest(String id, String username, UserPrefs userPrefs) {
+    super(id, username, userPrefs);
   }
 
   @Override
