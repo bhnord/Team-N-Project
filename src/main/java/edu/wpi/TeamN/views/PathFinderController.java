574ac6a67b454b5e9ea92a76cc70d809b8a25937
AlexMitchell1294 <amitchell2@wpi.edu>
2021-04-26 18:31:25 -0400

    added pathfinder UI

---
+++ src/main/java/edu/wpi/TeamN/views/PathFinderController.java
@@ -0,0 +1,37 @@
+package edu.wpi.TeamN.views;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.Initializable;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class PathFinderController implements Initializable {
+    private final MapController mapController = new MapController();
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+    }
+
+
+    public void exit(ActionEvent actionEvent) {
+    }
+
+    public void advanceViews(ActionEvent actionEvent) {
+    }
+
+    public void SetStartNode(ActionEvent actionEvent) {
+    }
+
+    public void SetEndNode(ActionEvent actionEvent) {
+    }
+
+    public void PathFind(ActionEvent actionEvent) {
+    }
+
+    public void setMap(ActionEvent actionEvent) {
+    }
+
+    public void logOut(ActionEvent actionEvent) {
+    }
+}
