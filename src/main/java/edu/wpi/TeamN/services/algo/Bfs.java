d0f15b4e863db9120401958b61e9d3099be909b6
finn <fwander@wpi.edu>
2021-04-26 14:56:49 -0400

    added pathfinding alts

--- src/main/java/edu/wpi/TeamN/services/algo/Bfs.java
+++ src/main/java/edu/wpi/TeamN/services/algo/Bfs.java
@@ -4,17 +4,34 @@ import java.util.*;
 
 public class Bfs implements  PathFinderI{
     @Override
-    public ArrayList<Node.Link> pathfind(Node start, Node end) {
-        return null;
-    }
-
-    @Override
-    public ArrayList<Node.Link> pathfindNoStairs(Node start, Node end) {
-        return null;
-    }
-
-    @Override
     public ArrayList<Node.Link> pathfindFull(Node start, Node end, Reduce filter) {
-        return null;
+        LinkedList<Node> open = new LinkedList<>();
+        ArrayList<Node> used = new ArrayList<>();
+        Node curnode;
+        used.add(start);
+        open.push(start);
+
+        while(!open.isEmpty()){
+            curnode = open.pop();
+            if (curnode == end){
+                break;
+            }
+            for (Node.Link l : curnode.get_neighbors()){
+                Node n = l._other;
+                if (!n.is_seen()){
+                    used.add(n);
+                    open.push(n);
+                    n.set_seen(true);
+                    n.set_parent(l);
+                }
+            }
+        }
+        ArrayList<Node.Link> ret = PathFinder.rebuild(start, end);
+        // reset manipulated nodes
+        for (Node n : used) {
+            n.reset();
+        }
+        return ret;
+
     }
 }
