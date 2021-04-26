06461fda4c1938aa673b887990ecb71caafe99cd
ananya <ananya.gopalan@gmail.com>
2021-04-26 17:45:21 -0400

    updated registratio page

--- src/main/java/edu/wpi/TeamN/views/RegisterNewUser.java
+++ src/main/java/edu/wpi/TeamN/views/RegisterNewUser.java
@@ -266,9 +266,7 @@ public class RegisterNewUser extends masterController implements Initializable {
               anchorPage.setEffect(blur);
               txtEmployeeName.setEffect(blur);
               popup1.show(
-                  confirmationStackPane,
-                  JFXPopup.PopupVPosition.BOTTOM,
-                  JFXPopup.PopupHPosition.LEFT);
+                  myStackPane, JFXPopup.PopupVPosition.BOTTOM, JFXPopup.PopupHPosition.LEFT);
               submit.setDisable(false);
             }
           });
