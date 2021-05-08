a5355328cb312c7801bb08c3f0c0385990a0147d
Michael Emerson <emerson.michael12@gmail.com>
2021-05-08 18:43:33 -0400

    Making camera close

--- src/main/java/edu/wpi/cs3733/d21/teamN/views/SideBarController.java
+++ src/main/java/edu/wpi/cs3733/d21/teamN/views/SideBarController.java
@@ -3,11 +3,9 @@ package edu.wpi.cs3733.d21.teamN.views;
 import com.google.inject.Inject;
 import com.jfoenix.controls.JFXColorPicker;
 import com.jfoenix.controls.JFXToggleButton;
+import edu.wpi.cs3733.d21.teamN.faceLogin.FaceEnroller;
 import edu.wpi.cs3733.d21.teamN.services.database.DatabaseService;
 import edu.wpi.cs3733.d21.teamN.services.database.users.User;
-import java.io.IOException;
-import java.net.URL;
-import java.util.ResourceBundle;
 import javafx.animation.TranslateTransition;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
@@ -24,6 +22,10 @@ import javafx.scene.paint.Color;
 import javafx.scene.shape.Rectangle;
 import javafx.util.Duration;
 import lombok.extern.slf4j.Slf4j;
+
+import java.io.IOException;
+import java.net.URL;
+import java.util.ResourceBundle;
 
 @Slf4j
 public class SideBarController extends MasterController implements Initializable {
@@ -52,6 +54,7 @@ public class SideBarController extends MasterController implements Initializable
   @FXML private JFXToggleButton darkModeSwitch;
 
   private User user;
+  private FaceEnroller faceEnroller;
 
   @FXML private AnchorPane SideAnchor;
   //  HomeControllerAdmin homeController;
@@ -180,6 +183,7 @@ public class SideBarController extends MasterController implements Initializable
   @FXML
   public void advanceHome() throws IOException {
     open = false;
+
     super.advanceHome(loader, appPrimaryScene);
   }
 
