018816117e21b8eb2fe6858eb193bf01b914f6e7
finn <fwander@wpi.edu>
2021-05-10 16:44:42 -0400

    nevermind

--- src/main/java/edu/wpi/cs3733/d21/teamN/views/FormEditorController.java
+++ src/main/java/edu/wpi/cs3733/d21/teamN/views/FormEditorController.java
@@ -12,7 +12,6 @@ import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
-import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.HBox;
@@ -64,11 +63,11 @@ public class FormEditorController extends MasterController implements Initializa
   //  }
 
   public void submit(ActionEvent actionEvent) throws IOException {
+    //    Parent root = loader.load(getClass().getResourceAsStream("Templateform.fxml"));
+    //    appPrimaryScene.setRoot(root);
+    //    FormController formController = loader.getController();
+    //    formController.setUp(form);
     db.updateForm(form);
-    Parent root = loader.load(getClass().getResourceAsStream("Templateform.fxml"));
-    appPrimaryScene.setRoot(root);
-    FormController formController = loader.getController();
-    formController.setUp(form.getForm());
   }
 
   public void add(ActionEvent actionEvent) {
